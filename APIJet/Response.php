066edd5914fb3c9f908eec5455d5ab1de700863f
Gyuner Zeki <me@gzeki.com>
2015-06-14 20:40:02 +0300

    Add send headers and body for response

--- APIJet/Response.php
+++ APIJet/Response.php
@@ -10,15 +10,23 @@ class Response
     const CODE_404  = '';
     const CODE_200  = '';
     
-    private static $code;
+    private static $code = 200;
+    private static $body = [];
+    private static $headers = [];
     
-    public static function setCode($code) {
-        // some validation for this code
+    public static function setCode($code) 
+    {
         self::$code = $code;
     }
     
-    public static function getCode() {
+    public static function getCode() 
+    {
         return self::$code;
+    }
+    
+    public static function setBody($body)
+    {
+        self::$body = $body;
     }
     
     public static function render()
@@ -29,12 +37,12 @@ class Response
     
     private static function sendHeaders()
     {
-        
+        http_response_code(self::getCode());
+        header('Content-type: application/json');
     }
     
     private static function sendBody()
     {
-        
+        echo json_encode(self::$body);
     }
-    
 }
