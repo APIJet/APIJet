df2fa1ba9dd017e388777530db3c958d80db2022
Gyuner Zeki <me@gzeki.com>
2015-06-16 20:14:27 +0300

    Refactoring APIJet\Config.

--- APIJet/Config.php
+++ APIJet/Config.php
@@ -4,17 +4,36 @@ namespace APIJet;
 
 class Config
 { 
-    private static $config = [];
+    private static $configStore = [];
+    private static $baseConfigDir = null;
     
     private function __construct() {}
     private function __clone() {}
     
+    /**
+     * @desc if config with corresponding file doesn't exist will return an empty array
+     * @param sting $name
+     * @return array 
+     */
     public static function getByName($name)
     {
-        if (!isset(self::$config[$name])) {
-            self::$config[$name] = include APIJet::getRootDir().'Config'.DIRECTORY_SEPARATOR.$name.'.php';
+        if (!isset(self::$configStore[$name])) {
+            $configFile = @include self::getBaseConfigDir().$name.APIJet::fileExt;
+            
+            if ($configFile === false) {
+                $configFile = [];
+            }
+            self::$configStore[$name] = $configFile;
         }
         
-        return self::$config[$name];
+        return self::$configStore[$name];
+    }
+    
+    private static function getBaseConfigDir()
+    {
+        if (self::$baseConfigDir === null) {
+            self::$baseConfigDir = APIJet::getRootDir().'Config'.DIRECTORY_SEPARATOR;
+        }
+        return self::$baseConfigDir;
     }
 }
