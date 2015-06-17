a8ee6e8c311c23af0c9f4fc2ccfaf2d7182ff7e3
Gyuner Zeki <me@gzeki.com>
2015-06-17 19:50:39 +0300

    Add routers (reverted from commit 6ca010611e933da91cc8d15d3e05a3231ca44a86)

--- Config/Router.php
+++ Config/Router.php
@@ -7,12 +7,7 @@ return [
         '{id}' => '([0-9]+)',
     ],
     'routes' => [
-        'jobs/list' => [R::POST, 'jobs\list'],
-        'jobs/list/{id}' => [R::GET_PUT_DELETE, 'jobs\list'],
-        
-        'candidates/list' => [R::POST, 'candidates\list'],
-        'candidates/review/{id}' => [R::GET, 'candidates\review'],
-        'candidates/search/{id}' => [R::GET, 'candidates\search'],
+        'hello_world' => [R::GET, 'hello\world'],        
     ]
 ];
 
